72b4e997f9c09bbe6dc89124e8f97452bd170f57
u-sho <shouhei.uechi@gmail.com>
2020-02-04 15:48:14 +0900

    :green_heart: fix visit-path of test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,18 +1,18 @@
 describe('Sapper template app', () => {
 	beforeEach(() => {
-		cy.visit('/')
+		cy.visit('/portfolio')
 	});
 
 	it('has the correct <h1>', () => {
 		cy.contains('h1', 'Great success!')
 	});
 
-	it('navigates to /about', () => {
+	it('navigates to /portfolio/about', () => {
 		cy.get('nav a').contains('about').click();
 		cy.url().should('include', '/about');
 	});
 
-	it('navigates to /blog', () => {
+	it('navigates to /portfolio/blog', () => {
 		cy.get('nav a').contains('blog').click();
 		cy.url().should('include', '/blog');
 	});
